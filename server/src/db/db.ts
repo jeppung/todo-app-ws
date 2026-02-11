d48b6f5dd68c3dd05a2e344cb0456dbd809ef151
jepung <jeppung@gmail.com>
2026-02-11 11:20:56 +0700

    feat: adding db initial configuration

---
+++ server/src/db/db.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
+}
+
+export const client = postgres(process.env.DATABASE_URL, { prepare: false });
+export const db = drizzle(client);
