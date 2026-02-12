d2b6887a4ba6c979cbcdb25469a637faf99a8802
jepung <jeppung@gmail.com>
2026-02-12 13:31:24 +0700

    feat: adding initial routing configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+import TodoDetail from "./components/TodoDetail.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="todos/:id" element={<TodoDetail />} />
+        <Route
+          path="*"
+          element={<h1 className="text-3xl font-semibold">404 Not Found</h1>}
+        />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
